8461d69e57789273bf8834e4580a43168e2c68b7
SukumarSivasubramaniyam <162662997+Sukueng@users.noreply.github.com>
2024-03-31 07:51:33 +0530

    icon links updated

--- src/components/HeadComponent.jsx
+++ src/components/HeadComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion"
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import zoro from "../assets/zoro.png"
+import resume from "../assets/sukumar sivasubramaniyam.pdf"
 import zoro2 from "../assets/zoropng.png"
 import pic1 from "../assets/pic1.png"
 import profile from "../assets/pp1.png"
@@ -22,8 +23,16 @@ function HeadComponent() {
         deleteSpeed: 40
     })
 
-    //animate={{ scale: 1 }} initial={{ scale: 0 }} transition={{ type: "tween", duration: 0.4 }}
+    const handleDownloadPdf = () => {
+        const link = document.createElement('a');
+        link.href = resume;
+        link.download = 'SukumarSivasubramaniyam.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
 
+    
 
     return (
         <>
@@ -39,12 +48,12 @@ function HeadComponent() {
                     <img className='roundimage' src={pic1} style={{ position: 'absolute', opacity: '0.5', zIndex: '-1', height: '90%', width: '50%', top: '40px', left: '-70px' }}></img>
                     <h4 className='mt-3 ' style={{ letterSpacing: '2px', color: 'white' }}>I'm a <span style={{ color: '#D919E8', letterSpacing: '2px' }}>{effect}<Cursor></Cursor></span></h4>
                     <button className='mt-3 btn ' style={{ borderRadius:'17px',border:'1px solid white' ,fontWeight:'bold',color:'white'}} onClick={()=>navigate("/about")}>About me &nbsp;<i class="bi bi-arrow-down  " style={{ color: 'white' }}> </i></button> &nbsp; &nbsp;
-                    <button className='mt-3 btn text-white' style={{ borderRadius:'17px',border:'1px solid black' ,fontWeight:'bold',background:'black'}}>Download CV &nbsp;</button><br></br>
+                    <button className='mt-3 btn text-white' style={{ borderRadius:'17px',border:'1px solid black' ,fontWeight:'bold',background:'black'}} onClick={()=>handleDownloadPdf()}>Download CV &nbsp;</button><br></br>
 
                     <div className="row mt-3 ">
                         <div className="col">
-                            <i class="bi bi-github p-2 ms-5" style={{ fontSize: '1.5rem' ,marginLeft:'10px',color:'white'}}></i>
-                            <i class="bi bi-linkedin p-2 ms-5" style={{ fontSize: '1.5rem',marginLeft:'10px',color:'white' }}></i>
+                            <a href='https://github.com/Sukueng/' target="_blank"><i class="bi bi-github p-2 ms-5" style={{ fontSize: '1.5rem' ,marginLeft:'10px',color:'white'}}></i></a>
+                            <a href="https://www.linkedin.com/in/sukumar-s-8846b6233?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"><i class="bi bi-linkedin p-2 ms-5" style={{ fontSize: '1.5rem',marginLeft:'10px',color:'white' }}></i></a>
                         </div>
                     </div>
 
